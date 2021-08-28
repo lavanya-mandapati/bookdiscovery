203d23a2c3e8a485d2c96ed1b66d9aeff3cbc162
svc <ashwin.kumar@zemosolabs.com>
2021-08-28 14:10:01 +0530

    added color palette and typography in themes

---
+++ src/theme/constants.ts
@@ -0,0 +1,30 @@
+export interface ColorMap {
+    [name: string]: string;
+}
+
+export const COLORS: ColorMap = {
+    PRIMARY_COLOR_300: "#70b0f3",
+    PRIMARY_COLOR_500: "#1665d8",
+    PRIMARY_COLOR_600: "#0b399b",
+    PRIMARY_COLOR_700: "#041c67",
+    PRIMARY_COLOR_100: "#cfe9fd",
+    GREY_900: "#2d3e50",
+    GREY_100: "#f5f8fa",
+    GREY_500: "#99acc2",
+    GREY_700: "#516f90",
+    GREY_300: "#dfe3eb",
+    WARNING_500: "#facf55",
+    ERROR_500: "#e6492d",
+    WHITE_50: "#fff",
+    SUCCESS_500: "#34aa44",
+    TRANSPARENT_0: "rgba(255, 255, 255, 0.01)",
+    TRANSPARENT_50: "rgba(255, 255, 255, 0.5)",
+    ICON_500: "#9ea0a5",
+    SECONDARY_100: "#e2f1f8",
+    SECONDARY_900: "#0c1938",
+    SECONDARY_500: "#425b76",
+    SECONDARY_300: "#9dbdd5",
+    SECONDARY_700: "#213554",
+    CHARCOAL_GREY: "#3e3f42",
+
+};
