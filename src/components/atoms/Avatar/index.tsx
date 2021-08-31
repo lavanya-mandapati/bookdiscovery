b76791546c9c3ccd9f330fea6e7da5ca3150f45d
rohitsaipandugolla <rohitsai.pandugolla@zemosolabs.com>
2021-08-31 10:19:42 +0530

    BBD-7_ProfileAvatar made suggested changes

--- src/components/atoms/Avatar/index.tsx
+++ src/components/atoms/Avatar/index.tsx
@@ -1,20 +1,15 @@
 import Avatar from '@material-ui/core/Avatar';
-
-
-
+  
 export type AvatarProps={
-    image?:string;
-    letter?:string;
+    src?:string;
+    children?:string;
     className?:string;
+    variant?:'rounded'|'square'|'circular'|'circle';
 };
   
-const AvatarAtom:React.FC<AvatarProps>= ({image,letter,className}) => {
-
+const AvatarAtom:React.FC<AvatarProps>= ({src,children,variant,className}) => {
     return (
-        <>
-        {!letter && <Avatar alt="userAvatar" src={image} className={className} />}
-        {letter && <Avatar className={className} >{letter}</Avatar>}
-        </>
+        <Avatar alt="User Avatar" src={src} variant={variant} className={className} children={children}/>
     );
 }
 
